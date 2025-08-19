52a4c4996dcaa4f8b2a53b8e9848dafad0d02749
ThunderVolt45 <92630716+ThunderVolt45@users.noreply.github.com>
2025-08-19 14:57:11 +0900

    최소 FPS 옵션 추가

--- Assets/UGUIWindowSample/Scripts/Sample/UGUIApplicationSetting.cs
+++ Assets/UGUIWindowSample/Scripts/Sample/UGUIApplicationSetting.cs
@@ -9,9 +9,16 @@ namespace UGUIWindow
     public class UGUIApplicationSetting : UGUIWindow
     {
         [Header("Application Setting")]
-        [SerializeField] private Vector2 minimumResolution;
+        [Tooltip("지원할 최소 해상도")]
+        [SerializeField] private Vector2 minimumResolution = new Vector2(1280, 720);
 
-        [Header("Setting Dropdown")]
+        [Tooltip("지원할 최소 FPS")]
+        [SerializeField] [Range(10, 1000)] private int minimumFPS = 60;
+
+        [Tooltip("지원할 DPI 설정 (1f = 100%)")]
+        [SerializeField] [Range(1f, 4f)] private float[] supportDPI;
+
+        [Header("UI Dropdown")]
         [SerializeField] private TMP_Dropdown dropdownWindowMode;
         [SerializeField] private TMP_Dropdown dropdownResolution;
         [SerializeField] private TMP_Dropdown dropdownFramerate;
@@ -56,32 +63,38 @@ namespace UGUIWindow
             // 해상도 리스트와 프레임레이트 리스트
             foreach (var resolution in Screen.resolutions)
             {
-                // 최저 해상도 보다 크면서 화면비는 가로가 세로보다 긴 비율의 해상도만 추가한다.
-                if (resolution.width >= minimumResolution.x && resolution.height >= minimumResolution.y
-                    && resolution.width > resolution.height)
+                // 최소 해상도보다 작은 해상도 옵션은 제거한다.
+                if (resolution.width < minimumResolution.x) continue;
+                if (resolution.height < minimumResolution.y) continue;
+
+                // 세로 화면비 해상도 옵션은 제거한다.
+                if (resolution.width < resolution.height) continue;
+
+                // 최소 FPS보다 낮은 해상도 옵션은 제거한다.
+                if (resolution.refreshRateRatio.value < minimumFPS - 1) continue;
+
+                string res = $"{resolution.width}x{resolution.height}";
+                string fps = $"{(int)Math.Round(resolution.refreshRateRatio.value)}Hz";
+
+                if (!resolutionList.Contains(res))
                 {
-                    string res = $"{resolution.width}x{resolution.height}";
-                    string fps = $"{(int)Math.Round(resolution.refreshRateRatio.value)}Hz";
+                    resolutionList.Add(res);
+                }
 
-                    if (!resolutionList.Contains(res))
-                    {
-                        resolutionList.Add(res);
-                    }
-
-                    if (!framerateList.Contains(fps))
-                    {
-                        framerateList.Add(fps);
-                    }
+                if (!framerateList.Contains(fps))
+                {
+                    framerateList.Add(fps);
                 }
             }
 
             // DPI 리스트
-            DPIList.Add("100%");
-            DPIList.Add("150%");
-            DPIList.Add("200%");
+            for (int i = 0; i < supportDPI.Length; i++)
+            {
+                DPIList.Add($"{(int)(supportDPI[i] * 100)}%");
+            }
 
             // 각 드롭다운 초기화
-            dropdownWindowMode.ClearOptions();
+                dropdownWindowMode.ClearOptions();
             dropdownResolution.ClearOptions();
             dropdownFramerate.ClearOptions();
             dropdownDPI.ClearOptions();
@@ -154,17 +167,12 @@ namespace UGUIWindow
             }
 
             // 현재 DPI 값을 찾아서 선택
-            switch (targetDPI)
+            for (int i = 0; i < supportDPI.Length; i++)
             {
-                case 1f:
-                    dropdownDPI.SetValueWithoutNotify(0);
-                    break;
-                case 1.5f:
-                    dropdownDPI.SetValueWithoutNotify(1);
-                    break;
-                default:
-                    dropdownDPI.SetValueWithoutNotify(2);
-                    break;
+                if (targetDPI.Equals(supportDPI[i]))
+                {
+                    dropdownDPI.SetValueWithoutNotify(i);
+                }
             }
         }
         #endregion
@@ -220,18 +228,7 @@ namespace UGUIWindow
 
         private void OnDPIChanged(int value)
         {
-            switch (value)
-            {
-                case 0:
-                    targetDPI = 1f;
-                    break;
-                case 1:
-                    targetDPI = 1.5f;
-                    break;
-                default:
-                    targetDPI = 2f;
-                    break;
-            }
+            targetDPI = supportDPI[value];
         }
         #endregion
 
